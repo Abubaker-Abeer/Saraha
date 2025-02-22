6459e96fffca95ac044cfa01690fbfeecb20d6fb
abeer-abubaker <abeerabubaker2001@gmail.com>
2025-02-22 21:51:58 +0200

    Refactored database connection to use environment variables (.env)

--- DB/connection.js
+++ DB/connection.js
@@ -1,8 +1,18 @@
+
 import { Sequelize } from 'sequelize';
-export const sequelize = new Sequelize('Saraha', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE, 
+  process.env.MYSQL_USER,      
+  process.env.MYSQL_PASSWORD,  
+  {
+    host: process.env.MYSQL_HOST,   
+    dialect: 'mysql',
+  }
+);
+
 export const connectdb = async () => {
   try {
     await sequelize.authenticate();
